3ad12209e8d2607ca087c03cd4ecad8e8f1eb000
lanxiangming <a0135858@u.nus.edu>
2015-04-19 12:24:45 +0800

    lay architectural course registration system

---
+++ CR_PL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CR_PL.MvcApplication" Language="C#" %>
---
+++ CR_PL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CR_PL.Startup))]
+namespace CR_PL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
