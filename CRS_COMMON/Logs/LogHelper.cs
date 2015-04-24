e93813c1b06e4c2cf01be820276f40f44df4ba3c
lyhistory <lyhistory@gmail.com>
2015-04-24 23:17:38 +0800

    add cross cutting project

---
+++ CRS_COMMON/Logs/LogHelper.cs
@@ -0,0 +1,48 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CRS_COMMON.Logs
+{
+
+    public class LogHelper
+    {
+        private ILog logger;
+
+        static LogHelper()
+        {
+
+            string path = HttpContext.Current.Server.MapPath("/");
+
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(Path.Combine(path, "Config","log4net.xml")));
+        }
+
+        private LogHelper(Type _type)
+        {
+            logger = LogManager.GetLogger(_type);
+        }
+
+        public static LogHelper GetLogger(Type _type)
+        {
+            return new LogHelper(_type);
+        }
+
+        public void Debug(string _Message, params object[] objs)
+        {
+            if (logger.IsDebugEnabled)
+            {
+                if (objs == null || objs.Length == 0)
+                {
+                    logger.Debug("DEBUG:" + _Message);
+                    return;
+                }
+                logger.Debug("DEBUG:" + string.Format(_Message, objs));
+            }
+        }
+    }
+}
---
+++ CRS_COMMON/Security/FormAuthentication/CRSFormsPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace CRS_COMMON.Security.FormAuthentication
+{
+    public class CRSFormsPrincipal<T> : IPrincipal where T : class,new()
+    {
+        public IIdentity Identity { get; private set; }
+        public T UserData { get; private set; }
+
+        public CRSFormsPrincipal(FormsAuthenticationTicket ticket, T userData)
+        {
+            if (ticket == null)
+            {
+                throw new ArgumentNullException("ticket");
+            }
+            if (userData == null)
+            {
+                throw new ArgumentNullException("userData");
+            }
+
+            Identity = new FormsIdentity(ticket);
+        }
+
+        public bool IsInRole(string role)
+        {
+            return false;
+        }
+        public bool IsInUser(string user)
+        {
+            return false;
+        }
+    }
+}
