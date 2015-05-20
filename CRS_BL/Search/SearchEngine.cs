ef962b234c9a9d63e64d8939367b8d7134948466
DancingOnAir <A0135925@u.nus.edu>
2015-05-21 00:55:51 +0800

    implement solr search function

--- CRS_BL/Search/SearchEngine.cs
+++ CRS_BL/Search/SearchEngine.cs
@@ -8,21 +8,35 @@ namespace nus.iss.crs.bl
 {
     internal class SearchEngine : ISearch
     {
+        SearchEngine()
+        {
+            
+        }
         /// <summary>
-        /// Search Registration by ame, id number, company,
+        /// Search Registration by name, id number, company,
         /// Search user
         /// search company
         /// search course by TITLE, CATEGORY, DESCRIPTION,INSTRUCTOR
         /// search class
         /// search instructor by name
         /// fulltext search
-        /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="criteion"></param>
         /// <returns></returns>
         public List<T> Search<T>(Search.SearchCriterion criteion)
         {
+            try 
+            {
+                string searchUrl = "http://localhost:8983/solr/CRS/select?wt=json&indent=true&q=name:" + criteion;
+                //HttpRequestMessage reuestMessage = new HttpRequestMessage(HttpMethod.Get, new Uri(relativeUrl, UriKind.Relative));
+                //reqMsg.Headers.UserAgent.TryParseAdd("MSIClient");
+                //HttpResponseMessage resMsg = await client.SendAsync(reqMsg);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Search error:" + ex);
+            }
             throw new NotImplementedException();
         }
     }
