d7e0534e278be9d1c3fe32bb2ab9d9e29069b528
DancingOnAir <A0135925@u.nus.edu>
2015-05-23 02:52:25 +0800

    add Json Person Converter

---
+++ CRS_BL/Search/Converter.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nus.iss.crs.bl.Search
+{
+    public abstract class Converter<T> : JsonConverter
+    {
+        protected abstract T Create(Type objectType,JObject jObject);
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(T).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader,
+                                        Type objectType,
+                                         object existingValue,
+                                         JsonSerializer serializer)
+        {
+            // Load JObject from stream
+            JObject jObject = JObject.Load(reader);
+
+            // Create target object based on JObject
+            T target = Create(objectType, jObject);
+
+            // Populate the object properties
+            serializer.Populate(jObject.CreateReader(), target);
+
+            return target;
+        }
+
+        public override void WriteJson(JsonWriter writer,
+                                       object value,
+                                       JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CRS_BL/Search/PersonConverter.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using nus.iss.crs.dm.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nus.iss.crs.bl.Search
+{
+    class PersonConverter : Converter<Registration>
+    {
+        protected override Registration Create(Type objectType, JObject jObject)
+        {
+            //if (FieldExists("Skill", jObject))
+            //{
+            //    return new Artist();
+            //}
+            //else if (FieldExists("Department", jObject))
+            //{
+            //    return new Employee();
+            //}
+            //else
+            //{
+            //    return new Person();
+            //}
+            return new Registration();
+        }
+
+        private bool FieldExists(string fieldName, JObject jObject)
+        {
+            return jObject[fieldName] != null;
+        }
+    }
+}
