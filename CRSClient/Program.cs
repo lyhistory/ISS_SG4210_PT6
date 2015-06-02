cd5962e239ac9b04315ad7948d71bd753813f120
lanxiangming <a0135858@u.nus.edu>
2015-06-02 17:57:26 +0800

    create course registration service

--- CRSClient/Program.cs
+++ CRSClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRSClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace WFClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CourseConfirmation());
+            Application.Run(new MainForm());
             //Application.Run(new Form1());
         }
     }
---
+++ CRS_PL/Service/CourseRegistrationService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace nus.iss.crs.pl.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CourseRegistrationService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CourseRegistrationService.svc or CourseRegistrationService.svc.cs at the Solution Explorer and start debugging.
+    public class CourseRegistrationService : ICourseRegistrationService
+    { 
+
+        public void DoWork()
+        {
+            Console.WriteLine("Do work");
+        }
+
+        public void RegistrateCourse(dm.Registration.Registration reg)
+        {
+            Console.WriteLine("DietaryRequirement:" + reg.DietaryRequirement);
+        }
+    }
+}
